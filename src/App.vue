99b34ee2ee6a3524a55edf7911405a570e2bdc1b
patti <patti.pujol93@gmail.com>
2021-07-17 11:09:45 +0200

    L-4 finish

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,10 @@
 <template>
   <div id="app">
     <div id="nav">
-      <router-link to="/">Home</router-link> |
+      <router-link to="/">Events</router-link> |
       <router-link to="/about">About</router-link>
     </div>
+    <h1>Events for Unicorns LOVERS</h1>
     <router-view />
   </div>
 </template>
---
+++ src/views/EventList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div class="events">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <eventCard  v-for="event in events" :key="event.id" :event="event"/>
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import eventCard from '@/components/eventCard.vue'
+
+export default {
+  name: 'EventList',
+  components: {
+    eventCard
+  },
+  data(){
+    return{
+      events: [
+        {
+         id: 5928101,
+          category: 'animal welfare',
+          title: 'Pinky Day',
+          description: 'Find your new feline friend at this event.',
+          location: 'Meow Town',
+          date: 'January 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee'
+      },
+      {
+         id: 5928102,
+          category: 'animal welfare',
+          title: 'Rainbow Week',
+          description: 'Find your new feline friend at this event.',
+          location: 'Meow Town',
+          date: 'January 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee'
+      },
+      {
+         id: 5928103,
+          category: 'animal welfare',
+          title: 'Fluffy ',
+          description: 'Find your new feline friend at this event.',
+          location: 'Meow Town',
+          date: 'January 28, 2022',
+          time: '12:00',
+          petsAllowed: true,
+          organizer: 'Kat Laydee'
+      }
+      ]
+    }
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+</style>
