dcb6b4cd224102984881a7488001f4b67ad7ca0d
patti <patti.pujol93@gmail.com>
2021-08-11 10:24:22 +0200

    commit deploy render

--- src/views/EventDetail.vue
+++ src/views/EventDetail.vue
@@ -1,13 +1,13 @@
 <template>
   <div class="event" :class="{'event-placeholder': !event}">
     <div v-if="event">
-        <h1>{{ event.title }} [{{category}}]</h1>
-        <p>{{ event.location }}</p>
-        <p>{{ eventDateTime }}</p>
-        <p>{{ event.description }}</p>
+      <h1>{{ event.title }} [{{category}}]</h1>
+      <p>{{ event.location }}</p>
+      <p>{{ eventDateTime }}</p>
+      <p>{{ event.description }}</p>
     </div>
     <div v-else>
-        <p>Loading...</p>
+      <p>Loading...</p>
     </div>
   </div>
 </template>
@@ -49,22 +49,23 @@ computed: {
 
 <style scoped>
 
- .event{
-     transition: all 0.4s ease-in;
-     background-color: white;
-     color: black;
-     width: 100%;
-     height: auto;
-     margin: 0 auto;
- }
-  .event.event-placeholder {
-     background-color: greenyellow;
-     color: indigo;
-     font-size: 20px;
-     font-weight: bold;
-     display: inline-block;
-     width: 400px;
-     height: 200px;
-     line-height: 180px;
- }
+    .event{
+        transition: all 0.4s ease-in;
+        background-color: white;
+        color: black;
+        width: 100%;
+        height: auto;
+        margin: 0 auto;
+    }
+    .event.event-placeholder {
+        background-color: greenyellow;
+        color: indigo;
+        font-size: 20px;
+        font-weight: bold;
+        display: inline-block;
+        width: 400px;
+        height: 200px;
+        line-height: 180px;
+    }
+
 </style>
